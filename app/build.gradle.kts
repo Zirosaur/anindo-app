014f849920d4d9eabd197dbeeb2295548ab66d00
ziro <ziro@local>
2026-09-14 19:14:31 +0700

    chore: update versionName to 0.1.0-beta and enhance release workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,8 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1.0-alpha"
+        versionName = "0.1.0-beta"
+
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
